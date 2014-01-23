77d5bfee42a0ad8a09b7e7b27b3804195d3b14ea
Kevin <kevin_r_s_ch@hotmail.com>
2014-01-23 15:59:53 -0500

    NumberPicker

--- StepTracker/src/espol/fiec/steptracker/MainActivity.java
+++ StepTracker/src/espol/fiec/steptracker/MainActivity.java
@@ -3,13 +3,30 @@ package espol.fiec.steptracker;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.NumberPicker;
 
 public class MainActivity extends Activity {
-
+	 NumberPicker nP;
+	 String[] min = new String[10];
+	 	 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        for (int i=0;i<10;i++){
+        	min[i]=Integer.toString(i+1);
+        }
+        
+        nP = (NumberPicker)findViewById(R.id.nPminutos);
+        nP.setMinValue(1);
+        nP.setMaxValue(10);
+        nP.setWrapSelectorWheel(false);
+        nP.setDisplayedValues(min);
+        
+        
+        
+        
     }
 
 
