bdc37570d54ad4ee50dbebd785529bdffff80fa4
stecandr <stecandr@espol.edu.ec>
2014-02-14 11:45:05 -0500

    Base de datos

---
+++ StepTracker/src/espol/fiec/steptracker/Dia.java
@@ -0,0 +1,60 @@
+public class Dia {
+ 
+    int id;
+    String fecha;
+    double tiempo;
+    double distancia;
+ 
+    // constructors
+    public Dia() {
+    }
+ 
+    public Dia(String fecha) {
+        this.fecha = fecha;
+        this.tiempo=0;
+        this.distancia=0;
+    }
+ 
+    public Dia(int id, String fecha) {
+        this.id = id;
+        this.fecha = fecha;
+        this.tiempo=0;
+        this.distancia=0;
+    }
+ 
+    // setters
+    public void setId(int id) {
+        this.id = id;
+    }
+ 
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+ 
+    public void setTiempo(double tiempo) {
+        this.tiempo = tiempo;
+    }
+     
+    public void setDistancia(double distancia){
+        this.distancia = distancia;
+    }
+ 
+    // getters
+    public long getId() {
+        return this.id;
+    }
+ 
+    public String getFecha() {
+        return this.fecha;
+    }
+ 
+    public double getDistancia() {
+        return this.distancia;
+    }
+
+    public double getTiempo() {
+        return this.tiempo;
+    }
+
+
+}
